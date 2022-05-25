9cdeaaffd04d198658cf6932c152697f96a237bf
JesseStuy20 <jxie20@stuy.edu>
2022-05-25 10:12:50 -0400

    partly working snake

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,30 @@
-var c = document.getElementById("canvas"); // GET CANVAS
+var c = document.querySelector('canvas'); // GET CANVAS
 var ctx = c.getContext('2d');
+
+angleChange = 0
+
+function keyDown(e) {
+  key = e.keyCode ;
+  if (key == 65) {
+    angleChange-- ;
+  }
+  if (key == 68) {
+    angleChange++ ;
+  }
+}
+
+function keyUp(e) {
+  key = e.keyCode ;
+  if (key == 65) {
+    angleChange++ ;
+  }
+  if (key == 68) {
+    angleChange-- ;
+  }
+}
+
+document.addEventListener("keydown",keyDown);
+document.addEventListener("keyup",keyUp);
 
 class SnakeSegment {
   constructor(name,type,color,x,y,hp) {
@@ -8,6 +33,8 @@ class SnakeSegment {
     this.color = color ;
     this.x = x ;
     this.y = y ;
+    this.lastX = x ;
+    this.lastY = y ;
     this.hp = hp ;
   }
   getXY() {
@@ -26,12 +53,41 @@ class Snake {
     this.speed = speed ;
   }
   addSegment(snakeSegment) {
-    this.segments.append(snakeSegment)
+    this.segments.append(snakeSegment) ;
   }
   moveSnake() {
-    xy = this.segments[0].getXY() ;
-    this.segments[0].move(xy[0]+Math.Cos(this.angle*(Math.PI/180)))
+    for(var i=0; i<this.segments.length;i++) {
+      this.segments[i].lastX = this.segments[i].x ;
+      this.segments[i].lastY = this.segments[i].y ;
+    }
+    var xy = this.segments[0].getXY() ;
+    this.segments[0].move(xy[0]+Math.cos(this.angle*(Math.PI/180)),xy[1]+Math.sin(this.angle*(Math.PI/180))) ;
+    for(var i=1; i<this.segments.length-1;i++) {
+      this.segments[i].move(this.segments[i-1].lastX,this.segments[i-1].lastY) ;
+    }
+  }
+  updateAngle() {
+    this.angle += angleChange ;
+  }
+  displaySnake() {
+    for(var i=0; i<this.segments.length;i++) {
+      var segment = this.segments[i] ;
+      ctx.beginPath();
+      ctx.arc(segment.x, segment.y, 80, 0, 2 * Math.PI);
+      ctx.fillStyle = segment.color ;
+      ctx.fill();
+    }
   }
 
 
 }
+let first = new SnakeSegment('bill','nye','green',100,100,100) ;
+let snake = new Snake(first,0,1) ;
+
+function display() {
+  ctx.clearRect(0,0,2000,1000) ;
+  snake.updateAngle() ;
+  snake.moveSnake() ;
+  snake.displaySnake() ;
+}
+setInterval(display, 10);

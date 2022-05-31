a2fec0e604331095b7b5086ebbb9919c3e799439
Josephine Lee <jlee25@stuy.edu>
2022-05-31 09:01:42 -0400

    db handling in separate file

---
+++ app/db.py
@@ -0,0 +1,27 @@
+# Epic Games | Jonathan Wu (Loki), Jesse Xie (Polly), William Chen (Cheap), Josephine Lee (Kitty)
+# SoftDev
+# P04 -- SoftDev Project 2021-22
+# 2022-05-24
+# 0 minutes
+
+import sqlite3
+DB_FILE = "leaderboard.db"
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+c = db.cursor()
+
+def create_tables():
+    c = db.cursor()
+    """Creates the tables in the database to store users"""
+    command = 'CREATE TABLE IF NOT EXISTS scores (user_id INTEGER, waveReached INTEGER, teamComposition TEXT)'
+    c.execute(command)
+    command = 'CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE)'
+    c.execute(command)
+    db.commit() #save changes
+create_tables()
+
+def create_user(username):
+    c = db.cursor()
+    create_tables()
+    """Adds a user with a username into the users table of the database"""
+    c.execute(f'INSERT INTO users (username) VALUES (?);', (username)) 
+    db.commit()

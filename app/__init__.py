8546b61abee633cc0a9b85a2d78a2e6a2615b585
Josephine Lee <56737932+josephinelee1234@users.noreply.github.com>
2022-06-01 09:20:02 -0400

    start leaderboard route

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,30 @@ def pythnx():
     db.create_tables()
     return render_template("index.html")
 
+#@app.route("/leaderboard", methods = ['GET', 'POST'])
+
+#def leaderboard():
+    #statement = ""
+    #if request.method == "POST":
+    #    if 'user' in request.form:
+    #        db = sqlite3.connect(MAIN_DB)
+    #        c = db.cursor()
+    #        c.execute("""
+    #            INSERT INTO LEADERBOARD (USER, SCORE, MODE) VALUES (?,?,?)
+    #            """, ((request.form['user'], request.form['score'], request.form['mode'])))
+    #        statement += """ 'request.form['mode']' '"""
+    #        db.commit()
+    #        db.close()
+    #        return redirect("/leaderboard")
+    #else:
+    #    statement = """ SELECT USER, SCORE FROM LEADERBOARD WHERE MODE = 'NORMAL' ORDER BY SCORE DESC"""
+    #db = sqlite3.connect(MAIN_DB)
+    #c = db.cursor()
+    #c.execute(statement)
+    #data = c.fetchall()
+    #db.close()
+    #return render_template("leaderboard.html", userScore = data, mode = "/leaderboard")
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()

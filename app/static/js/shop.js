0506866dfef9cb9f54120b5f088fc75f546755b8
JesseStuy20 <jxie20@stuy.edu>
2022-06-02 14:24:09 -0400

    Edited code to display test shop after start

--- app/static/js/shop.js
+++ app/static/js/shop.js
@@ -5,13 +5,28 @@ var ctx2 = c2.getContext('2d');
 var parent = document.getElementById("parent");
 c2.width = parent.offsetWidth;
 c2.height = window.innerHeight - (1.6 * parent.offsetHeight);
-let classList = ['Warrior','Archer','Mage','Healer'] ;
 
 function randomSegment() {
   let rand = (Math.random() * 4) ;
   if (rand == 0) {
-    var temp = new SnakeSegment('bill','Archer','LawnGreen',200,100,100) ;
-    return temp ;
+    return ['Ranger','LawnGreen'] ;
+  }
+  if (rand == 1) {
+    return ['Rogue','LightSalmon'] ;
+  }
+  if (rand == 2) {
+    return ['Warrior','Gold'] ;
+  }
+  if (rand == 3) {
+    return ['Mage','CornflowerBlue'] ;
   }
 
 }
+
+function display() {
+  if (localStorage.getItem('active') == 'shop') {
+    ctx2.font = "30px Arial";
+    ctx2.fillText("test shop",100,100);
+  }
+}
+setInterval(display,10);

f77a84508b6e0a7eafabc92296cc25c7102ca30a
Iamchwill <wchen20@stuy.edu>
2022-06-07 09:26:48 -0400

    ehhhhhh

--- app/static/js/snakes.js
+++ app/static/js/snakes.js
@@ -1,11 +1,11 @@
 class speedsters extends snakeSegment{
   constructor(level,x,y,hp,atk,atkSpeed) {
-    super("speedster", "speedsters", "FFFF33", level, x, y, 80 + hp, 20 + atk, 5 + atkSpeed);
+    super("speedster", "speedsters", "FFFF33", level, x, y, 80 + hp, 20 + atk, 20 - atkSpeed);
   }
 }
 
 class rangers extends snakeSegment{
   constructor(level, x, y, hp, atk){
-    super("ranger", "rangers", "FFFF33", level, x, y, 85 + hp, 15 + atk, 5 + atkSpeed);
+    super("ranger", "rangers", "FFFF33", level, x, y, 85 + hp, 15 + atk, atkSpeed);
   }
 }

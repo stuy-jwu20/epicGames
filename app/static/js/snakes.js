cb67172d618e6cbc25ca22387b19b9cf762633a6
iamchwill <wchen20@stuy.edu>
2022-06-09 23:05:09 -0400

    Snakes do things, but don't show it

--- app/static/js/snakes.js
+++ app/static/js/snakes.js
@@ -1,38 +1,54 @@
 import { SnakeSegment, Snake } from './snake.js';
 
+function dist(x1, y1, x2, y2){
+  var distance = Math.sqrt((Math.pow(x1 - x2,2))+(Math.pow(y1-y2,2)))
+  return distance;
+};
+
 class speedsters extends snakeSegment{
   constructor(level,x,y,hp,atk,atkSpeed) {
-    super("speedster", "speedsters", "FFFF33", level, x, y, 80 + hp, 20 + atk, 20 - atkSpeed);
+    super("speedster", "speedsters", "FFFF33", level, x, y, 80 + hp, 20 + atk, 4 - atkSpeed);
+  }
+
+  function shoot(){
+    wave.bugs[0].health -= atk;
   }
 }
 
 class rangers extends snakeSegment{
-  constructor(level, x, y, hp, atk){
-    super("ranger", "rangers", "FFFF33", level, x, y, 85 + hp, 15 + atk, atkSpeed);
+  constructor(level, x, y, hp, atk, atkSpeed){
+    super("ranger", "rangers", "2C9200", level, x, y, 85 + hp, 15 + atk, 6 - atkSpeed);
+  }
+
+  function shoot(){
+    wave.bugs[0].health -= atk;
   }
 }
 
 class supporters extends snakeSegment{
   constructor(level, x, y, hp, atk){
-    super("supporter", "supporters", "FFFF33", level, x, y, 85 + hp, 15 + atk, atkSpeed);
+    super("supporter", "supporters", "25E1D4", level, x, y, 85 + hp, 15 + atk, 0);
   }
 }
 
 class fighters extends snakeSegment{
   constructor(level, x, y, hp, atk){
-    super("fighter", "fighters", "FFFF33", level, x, y, 85 + hp, 15 + atk, atkSpeed);
+    super("fighter", "fighters", "FF4F00", level, x, y, 150 + hp, 30 + atk, 0);
   }
 }
 
 class nukers extends snakeSegment{
   constructor(level, x, y, hp, atk){
-    super("nuker", "nukers", "FFFF33", level, x, y, 85 + hp, 15 + atk, atkSpeed);
+    super("nuker", "nukers", "FF005A", level, x, y, 85 + hp, 15 + atk, 0);
   }
-}
 
-class pellet{
-  constructor(size, dmg){
-    this.dmg = dmg;
-    this.size = size;
+  function blast(){
+    x = wave.bugs[0].x;
+    y = wave.bugs[0].y;
+    for(bug in wave.bugs){
+      if(dist(x,y, bug.x, bug.y) < 6){
+        bug.health -= this.atk;
+      }
+    }
   }
 }

5185086acd50a3a74c892b0c6850d3288ee3814e
Jonathan Wu <jwu20@stuy.edu>
2022-06-10 03:27:01 -0400

    remove useless code

--- app/db.py
+++ app/db.py
@@ -18,22 +18,3 @@ def create_tables():
     c.execute(command)
     db.commit() #save changes
 create_tables()
-
-def create_user(username):
-    c = db.cursor()
-    create_tables()
-    """Adds a user with a username into the users table of the database"""
-    c.execute(f'INSERT INTO users (username) VALUES (?);', (username)) 
-    db.commit()
-
-def get_scores(user_id):
-    c = db.cursor()
-    result = list(c.execute(f'SELECT waveReached from scores where user_id == ?', (user_id,)))
-    return [{
-        "w": waveReached
-    } for (waveReached) in result][0]
-
-def inc_waveReached(user_id):
-    c = db.cursor()
-    c.execute(f'UPDATE scores SET waveReached = ? where user_id == ?', (get_scores(user_id)['w'] + 1, user_id))
-    db.commit()

af1d94f234b5836bd8344551c1ef8b7f3f926a77
iamchwill <wchen20@stuy.edu>
2022-06-10 00:23:12 -0400

    Added variants

--- app/static/js/snakes.js
+++ app/static/js/snakes.js
@@ -6,8 +6,8 @@ function dist(x1, y1, x2, y2){
 };
 
 class speedsters extends snakeSegment{
-  constructor(level,x,y,hp,atk,atkSpeed) {
-    super("speedster", "speedsters", "FFFF33", level, x, y, 80 + hp, 20 + atk, 3000 - atkSpeed);
+  constructor(name,level,x,y,hp,atk,atkSpeed) {
+    super(name, "speedsters", "FFFF33", level, x, y, 80 + hp, 20 + atk, 3000 - atkSpeed);
   }
 
   function shoot(){
@@ -19,12 +19,30 @@ class speedsters extends snakeSegment{
     ctx.moveTo(this.x, this.y);
     ctx.lineTo(wave.bugs[0].x, wave.bugs[0].y);
     wave.bugs[0].health -= atk;
+  }
+}
+
+class runner extends speedsters{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("runner", level, x, y, hp, atk, atkSpeed);
+  }
+}
+
+class maglev extends speedsters{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("maglev", level, x, y, hp, atk, atkSpeed);
+  }
+}
+
+class charger extends speedsters{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("charger", level, x, y, hp, atk, atkSpeed);
   }
 }
 
 class rangers extends snakeSegment{
-  constructor(level, x, y, hp, atk, atkSpeed){
-    super("ranger", "rangers", "2C9200", level, x, y, 85 + hp, 15 + atk, 5000 - atkSpeed);
+  constructor(name, level, x, y, hp, atk, atkSpeed){
+    super(name, "rangers", "2C9200", level, x, y, 85 + hp, 15 + atk, 5000 - atkSpeed);
   }
 
   function shoot(){
@@ -39,9 +57,45 @@ class rangers extends snakeSegment{
   }
 }
 
+class sniper extends rangers{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("sniper", level, x, y, hp, atk, atkSpeed);
+  }
+}
+
+class archer extends rangers{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("archer", level, x, y, hp, atk, atkSpeed);
+  }
+}
+
+class deadeye extends rangers{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("deadeye", level, x, y, hp, atk, atkSpeed);
+  }
+}
+
 class supporters extends snakeSegment{
-  constructor(level, x, y, hp, atk){
-    super("supporter", "supporters", "25E1D4", level, x, y, 85 + hp, 15 + atk, 0);
+  constructor(name, level, x, y, hp, atk){
+    super(name, "supporters", "25E1D4", level, x, y, 85 + hp, 15 + atk, 0);
+  }
+}
+
+class healer extends supporters{
+  constructor(level,x,y,hp,atk){
+    super("healer", level, x, y, hp, atk);
+  }
+}
+
+class priest extends supporters{
+  constructor(level,x,y,hp,atk){
+    super("priest", level, x, y, hp, atk);
+  }
+}
+
+class cultist extends supporters{
+  constructor(level,x,y,hp,atk){
+    super("cultist", level, x, y, hp, atk);
   }
 }
 
@@ -51,9 +105,27 @@ class fighters extends snakeSegment{
   }
 }
 
+class boxer extends fighters{
+  constructor(level,x,y,hp,atk){
+    super("boxer", level, x, y, hp, atk);
+  }
+}
+
+class knight extends fighters{
+  constructor(level,x,y,hp,atk){
+    super("knight", level, x, y, hp, atk);
+  }
+}
+
+class warrior extends fighters{
+  constructor(level,x,y,hp,atk){
+    super("warrior", level, x, y, hp, atk);
+  }
+}
+
 class nukers extends snakeSegment{
-  constructor(level, x, y, hp, atk){
-    super("nuker", "nukers", "FF005A", level, x, y, 85 + hp, 15 + atk, 0);
+  constructor(name, level, x, y, hp, atk, atkspeed){
+    super(name, "nukers", "FF005A", level, x, y, 85 + hp, 20 + atk, 6500 - atkSpeed);
   }
 
   function blast(){
@@ -64,5 +136,23 @@ class nukers extends snakeSegment{
         bug.health -= this.atk;
       }
     }
+  }
+}
+
+class boomer extends nukers{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("boomer", level, x, y, hp, atk, atkSpeed);
+  }
+}
+
+class blaster extends nukers{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("blaster", level, x, y, hp, atk, atkSpeed);
+  }
+}
+
+class breaker extends nukers{
+  constructor(level,x,y,hp,atk,atkSpeed){
+    super("breakers", level, x, y, hp, atk, atkSpeed);
   }
 }
